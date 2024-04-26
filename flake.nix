0034a1592719f28b83ed4f44ef854d63c94439af
zmrocze <karolochmanmilarski@gmail.com>
2024-04-26 20:20:31 +0200

    Try add superdirt-install but there's typo upstream

--- flake.nix
+++ flake.nix
@@ -5,13 +5,26 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     tidal = {
       url = "github:mitchmindtree/tidalcycles.nix";
+      # url = "github:zmrocze/tidalcycles.nix/superdirt-install";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
-  outputs = inputs:
-    inputs.tidal.utils.eachSupportedSystem (system: {
-      devShells = inputs.tidal.devShells.${system};
+  outputs = inputs@{ tidal, self, ... }:
+    let
+      pkgsFor = system: import inputs.nixpkgs {
+        system = system;
+        overlays = [ tidal.overlays.tidal ];
+      };
+    in 
+    tidal.utils.eachSupportedSystem (system: {
+      devShells = {
+        tidal = tidal.devShells.${system}.tidal.overrideAttrs (oldAttrs: {
+          buildInputs = oldAttrs.buildInputs ++ [
+            ((pkgsFor system).superdirt-install)
+          ];
+        });
+        default = self.devShells.${system}.tidal;
+      };
       formatter = inputs.tidal.formatter.${system};
     });
 }
